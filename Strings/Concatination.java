dfaff1cb30aecdcba37846426bb8a44c792d39f1
aryansrivastav05 <srivastavaryan182@gmail.com>
2025-08-28 23:39:14 +0530

    Concatination of Strings

---
+++ Strings/Concatination.java
@@ -0,0 +1,9 @@
+public class Concatination {
+    public static void main(String[] args) {
+        String firstName = "Aryan";
+        String SecondName = "Srivastava";
+        String FullName = firstName+" " + SecondName;
+        System.out.println(FullName);
+
+    }
+}
