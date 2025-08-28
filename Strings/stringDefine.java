b0568efae5d3c141a7e5d66a5e086b902ceadf3d
aryansrivastav05 <srivastavaryan182@gmail.com>
2025-08-28 23:17:15 +0530

    String Defination

---
+++ Strings/stringDefine.java
@@ -0,0 +1,11 @@
+public class stringDefine {
+    public static void main(String[] args) {
+        String str = "Aryan";
+        String str2 = new String("Aryan");
+        System.out.println(str2);
+
+        System.out.println(str);
+        str2 ="abc"; 
+        System.out.println(str2);
+    }
+}
---
+++ Strings/subString.java
@@ -0,0 +1,17 @@
+public class subString {
+    public static String substring(String str,int si,int ei){
+        String subStr = "";
+        for(int i=si;i<ei;i++)
+        {
+            subStr +=str.charAt(i);
+        }
+        return subStr;
+
+    }
+    public static void main(String[] args) {
+        String str = "Helloworld";
+        System.out.println(str.substring(0, 5));
+       // System.out.println(substring(str,0, 5));
+    }
+
+}
